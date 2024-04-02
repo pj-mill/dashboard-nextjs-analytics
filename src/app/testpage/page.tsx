cfbf55ec5e4ba2cda22c5429cbc4ff428764880e
@Apollo013 <paul.j.millar@gmail.com>
2024-04-02 11:58:32 +0100

    Completing Analytics View

---
+++ src/app/testpage/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Page = () => {
+    return <p>Test Page</p>;
+};
+
+export default Page;
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,19 +1,15 @@
 import { NextRequest, NextResponse } from "next/server";
-import { analytics } from "./utils/analytics";
+import { analytics } from "./api/analytics";
 
 export default async function middleware(req: NextRequest) {
-    // If navigating to the home page
+    // If navigating to the home page only
     if (req.nextUrl.pathname === "/") {
         // Track analytic events
         try {
-            await analytics.track(
-                "pageview",
-                {
-                    page: "/",
-                    country: req.geo?.country,
-                },
-                {}
-            );
+            await analytics.track("pageview", {
+                page: "/",
+                country: "gb",
+            });
         } catch (err) {
             //Fail silently
             console.error(err);
